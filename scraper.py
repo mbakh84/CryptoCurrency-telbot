e5f179a2b8e496d0f1e6659c969456828b3ecccd
M.Bakhshi <mbakh84@gmail.com>
2024-03-19 18:28:40 +0330

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,15 +3,18 @@ from selenium.webdriver.common.by import By
 from time import sleep
 import os
 import datetime
+#import libraries
 
 driver = webdriver.Chrome()
+#start webdriver
 
-count_of_cryptos = 8
+count_of_cryptos = 8 #count of cryptos which we extract thier datas
 
 crypto_ID = {78:"BTC",7841:"ETH",6746:"USDT",8665:"SOL",2649:"BNB",80:"XRP",6739:"USDC",3498:"DOGE"}
+#in the JSON type datas, we have access to a data with an specific ID; this dictionary save the IDs
 
 while(1):
-    price_list = {"BTC":0,"ETH":0,"USDT":0,"SOL":0,"BNB":0,"XRP":0,"USDC":0,"DOGE":0}
+    price_list = {"BTC":0,"ETH":0,"USDT":0,"SOL":0,"BNB":0,"XRP":0,"USDC":0,"DOGE":0 #this dictionary will save the extracted price
     for i in range(count_of_cryptos):
         driver.get(f"https://fcsapi.com/api-v3/crypto/latest?id={list(crypto_ID.keys())[i]}&access_key=8CNLQ22cKi2m2Evhq96xs")
         pre_element = driver.find_element(By.XPATH,"//pre")
